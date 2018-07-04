b0d45f3f7e41de8faf5fc5811631a8541cba58f6
cjbel <christopher.bellingham@googlemail.com>
2018-07-04 19:15:56 +0100

    Some preliminary code.

---
+++ blender_hand_drawn_npr/tutorial_examples/circle.py
@@ -0,0 +1,10 @@
+# Copied from:
+# http://scikit-image.org/docs/dev/api/skimage.draw.html#skimage.draw.circle
+
+import numpy as np
+from skimage.draw import circle
+img = np.zeros((10, 10), dtype=np.uint8)
+rr, cc = circle(4, 4, 5)
+img[rr, cc] = 1
+
+print(img)
