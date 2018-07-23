63328391018f07365209e97d2ca0570f5a64d8e8
cjbel <christopher.bellingham@googlemail.com>
2018-07-23 18:24:50 +0100

    Logging added.

--- blender_hand_drawn_npr/hand_drawn_npr.py
+++ blender_hand_drawn_npr/hand_drawn_npr.py
@@ -1,10 +1,25 @@
 import bpy
+import os
+import tempfile
+import logging
+
+# Log to a temporary directory in a platform-independent way.
+log_file = os.path.join(tempfile.gettempdir(), 'hand_drawn_npr.log')
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    filename=log_file,
+                    filemode='w')
 
 bl_info = {"name": "Hand Drawn NPR", "category": "Render"}
 
+print(bl_info['name'] + " logging path: " + log_file)
+
 
 class MainPanel(bpy.types.Panel):
-    """Creates a Panel in the Object properties window"""
+    """Create a Panel in the Render properties window."""
+
+    logging.debug("Instantiating MainPanel...")
+
     bl_label = "Hand Drawn NPR"
     bl_idname = "RENDER_PT_hand_drawn_main_panel"
     bl_space_type = 'PROPERTIES'
@@ -12,14 +27,19 @@ class MainPanel(bpy.types.Panel):
     bl_context = "render"
 
     def draw(self, context):
+        logging.debug("Drawing MainPanel...")  # draw() is called frequently during normal use, consider omitting this.
         self.layout.label(text="Lorem ipsum dolor sit amet...")
 
 
 def register():
+    logging.debug("Registering classes...")
+
     bpy.utils.register_class(MainPanel)
 
 
 def unregister():
+    logging.debug("Unregistering classes...")
+
     bpy.utils.unregister_class(MainPanel)
 
 
