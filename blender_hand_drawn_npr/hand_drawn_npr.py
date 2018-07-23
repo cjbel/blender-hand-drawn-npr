007e4d2335853c58460ba1a1ae66b9cfc151df33
cjbel <christopher.bellingham@googlemail.com>
2018-07-23 18:45:24 +0100

    Small corrections.

--- blender_hand_drawn_npr/hand_drawn_npr.py
+++ blender_hand_drawn_npr/hand_drawn_npr.py
@@ -4,15 +4,15 @@ import tempfile
 import logging
 
 # Log to a temporary directory in a platform-independent way.
-log_file = os.path.join(tempfile.gettempdir(), 'hand_drawn_npr.log')
+log_file = os.path.join(tempfile.gettempdir(), "hand_drawn_npr.log")
 logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)s %(message)s',
+                    format="%(asctime)s %(levelname)s %(message)s",
                     filename=log_file,
-                    filemode='w')
+                    filemode="w")
 
-bl_info = {"name": "Hand Drawn NPR", "category": "Render"}
+bl_info = {"name": "hand Drawn NPR", "category": "Render"}
 
-print(bl_info['name'] + " logging path: " + log_file)
+print(bl_info["name"] + " logging path: " + log_file)
 
 
 class MainPanel(bpy.types.Panel):
@@ -22,8 +22,8 @@ class MainPanel(bpy.types.Panel):
 
     bl_label = "Hand Drawn NPR"
     bl_idname = "RENDER_PT_hand_drawn_main_panel"
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = 'WINDOW'
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
     bl_context = "render"
 
     def draw(self, context):
