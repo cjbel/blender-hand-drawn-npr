da2398ac7fc7af18940190d4db0fd9cbcd6c1e1e
cjbel <christopher.bellingham@googlemail.com>
2018-07-23 21:26:20 +0100

    Fix

---
+++ install_addon.py
@@ -0,0 +1,7 @@
+"""
+Add-on install script for use with Travis CI.
+"""
+
+import bpy
+
+bpy.ops.wm.addon_install(filepath="/home/travis/build/cjbel/blender-hand-drawn-npr/blender_hand_drawn_npr/hand_drawn_npr.py")
