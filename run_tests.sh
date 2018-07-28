ad60f9b1875c8006a1742a51ba3a031f2e21dbba
cjbel <christopher.bellingham@googlemail.com>
2018-07-28 16:32:02 +0100

    CI tweaks

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Test suite must be run from within Blender. Output is noisy, so redirect it.
-blender -b -P tests/tests.py -noaudio 1> /dev/null
+blender -b -P tests/tests.py -noaudio
